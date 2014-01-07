c68ab9152787449b28a242af4ce91deab0c7b9dc
bluteau <matthew.bluteau@gmail.com>
2014-01-07 00:08:24 +0000

    Added tab class module to start object-oriented implementation

---
+++ tab.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 7, 2014
+
+@author: Matt
+'''
+
+class tab(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
