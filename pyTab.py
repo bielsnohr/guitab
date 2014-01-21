2729095e621cf8e76bd4b6d04c46fdfd876eb56d
bluteau <matthew.bluteau@gmail.com>
2014-01-21 21:01:42 +0000

    Initial full object-oriented implementation of pyTab

--- pyTab.py
+++ pyTab.py
@@ -12,16 +12,7 @@
 # appending to the input file given as below)
 
 import argparse
-import numpy as np
-import copy as cp
-import re
-
-# Set up a blank tab to be copied each time at the beginning of each iteration
-# of main()
-blank = np.zeros((6, 80), dtype='U2')
-blank[:, :] = '-'
-blank[:, 0] = ('e', 'B', 'G', 'D', 'A', 'E')
-blank[:, 1] = '|'
+import tab
 
 
 def main():
@@ -30,76 +21,77 @@ def main():
           "details of how to run.")
     allowed = ['-', 'h', 'p', 'x'] + [str(x) for x in range(25)]
     flag = True
-    newtab = True
-    i = 2
-    max_ind = max(blank.shape)
+    # define the argument parser object
+    parser = argparse.ArgumentParser(prog='pyTab', description='TO-DO:'
+                                     'fill in description of pyTab')
+    parser.add_argument('-c', '--chord', nargs=6, choices=allowed, help=
+                        'the chord/fingering for the current count (if'
+                        ' present, there must be 6 positional arguments'
+                        '-one for each string). Input order runs from high '
+                        'e to low e', default='-')
+                        #default=['-', '-', '-', '-', '-', '-'])
+    parser.add_argument('-d', action='store_true', help='flag'
+                        ' to quit pyTab and save progress')
+    parser.add_argument('-b', nargs='?', const=1, type=int, help='the'
+                        ' number of counts to go back in the tab '
+                        '(the default is 1 if the flag is present)')
+    parser.add_argument('-f', nargs='?', const=1, type=int, help='the'
+                        ' number of counts to go forward in the tab '
+                        '(the default is 1 if the flag is present)')
+    parser.add_argument('-o', type=argparse.FileType('a'),
+                        default='mytab.txt', help='the file for the tab'
+                        ' to be written to')
+    # URGENT: need to modify the argument parser so that the 6 positional
+    # arguments are optional (will probably have to make them optional
+    # arguments instead of positional)
+    # TO-DO: add an option for inputting a title, add an option for adding
+    # the author to the file, add option for quitting without saving
+    # progress
 
     while(True):
-        if newtab:
-            tab = cp.deepcopy(blank)
-            newtab = False
+        #if newtab:
+        #    tab = cp.deepcopy(blank)
+        #    newtab = False
 
         inp = input('[pyTab]: ')
 
-        parser = argparse.ArgumentParser(prog='pyTab', description='TO-DO:'
-                                         'fill in description of pyTab')
-        parser.add_argument('chord', nargs='*', choices=allowed, help=
-                            'the chord/fingering for the current count (if'
-                            ' present, there must be 6 positional arguments'
-                            '-one for each string). Input order runs from high '
-                            'e to low e', default='-')
-                            #default=['-', '-', '-', '-', '-', '-'])
-        parser.add_argument('-d', action='store_true', help='flag'
-                            ' to quit pyTab and save progress')
-        parser.add_argument('-b', nargs='?', const=1, type=int, help='the'
-                            ' number of counts to go back in the tab '
-                            '(the default is 1 if the flag is present)')
-        parser.add_argument('-o', type=argparse.FileType('a'),
-                            default='mytab.txt', help='the file for the tab'
-                            ' to be written to')
-        # TO-DO: add an option for inputting a title, add an option for adding
-        # the author to the file, add option for adding blank chord, 
         try:
             args = parser.parse_args(inp.split())
         except:
-            print("Your input wasn't correct. Please try again...")
             continue
 
         # could read in the input file and check for a header to see if a
         # header needs to be written
         if flag:
-            args.o.write("Header for pyTab Tab")
+            args.o.write("Header for pyTab Tab\n")
+            # initiate the tab for the first time
+            x = tab.tab()
             # TO-DO: add more information to this like author, filename, date
             flag = False
         if args.d:
             # write current tab buffer to file
-            tab_text = re.sub('(\[\[)|(\]\])|(\s)|(\')', '',
-                         str(tab)).replace('][', '\n')
-            args.o.write('\n' + tab_text)
+            args.o.write('\n' + str(x))
             args.o.close()
             break
+        if args.b is not None:
+            x.back(args.b)
+            continue
+        if args.f is not None:
+            x.forward(args.f)
+            continue
         elif len(args.chord) != 6:
             print('You must enter 6 positional arguments when specifying a'
                   ' count.\nPlease try again.')
             # Raise a type error here instead? This would necessitate a new
-            # class for lists of length 6?
+            # class for lists of length 6? Or just a simple type error...
             continue
         else:
-            # Now, we want to start writing the tab, but I need the numpy array
-            # template that I want to edit... what is the most efficient way
-            # to get this starting template? Use copy.deepcopy? or make a new
-            # class that defines a "clone" method and essentially just returns
-            # a copy of itself via this method...
-            tab[:, i] = args.chord
-            tab_text = re.sub('(\[\[)|(\]\])|(\s)|(\')', '',
-                         str(tab)).replace('][', '\n')
-            print(tab_text)
-            i = i + 1
-            if i > max_ind:
-                args.o.write('\n' + tab_text)
+            # Now, we want to write to the tab
+            if x.write(args.chord):
+                args.o.write('\n' + str(x))
                 print("Starting new line...")
-                newtab = True
-                i = 2
+                # reset the tab to initial settings
+                x.__init__()
 
 
 if __name__ == "__main__":
--- tab.py
+++ tab.py
@@ -3,15 +3,81 @@ Created on Jan 7, 2014
 
 @author: Matt
 '''
+import re
+import numpy as np
+import math
+
 
 class tab(object):
     '''
-    classdocs
+    Class for the textual display and modification of a guitar tab.
     '''
 
-
-    def __init__(selfparams):
+    def __init__(self):
         '''
         Constructor
         '''
-        
+        # the maximum length of a tab
+        self._MAX = 80
+        # the chord indicating the user's current location in the tab
+        self._current = ('*', '*', '*', '*', '*', '*')
+        # the numpy array that will hold the tab data
+        self.tab_data = np.zeros((6, self._MAX), dtype='U2')
+        self.tab_data[:, :] = '-'
+        self.tab_data[:, 0] = ('e', 'B', 'G', 'D', 'A', 'E')
+        self.tab_data[:, 1] = '|'
+        # the index for the current column to be written to
+        self.i = 2
+        # the default name for the file to be written to
+        self.file = 'myTab.txt'
+
+    def __str__(self):
+        # this convoluted statement formats the numpy array so it looks like a
+        # nice tab
+        return re.sub('(\[\[)|(\]\])|(\s)|(\')', '',
+                      str(self.tab_data)).replace('][', '\n')
+
+    def write(self, chord):
+        '''
+        Writes the input chord to the current tab. If the tab is full, the
+        function returns True, and otherwise it returns False.
+        '''
+        # Implement a check for the length of chord?
+        self.tab_data[:, self.i] = chord
+        print(self)
+        self.i += 1
+        if self.i >= self._MAX:
+            return True
+        else:
+            return False
+
+    def back(self, num):
+        if self.i - abs(num) < 2:
+            print("Index requested out of bounds; please try again.")
+        else:
+            self.i -= abs(num)
+            self.tab_data[:, self.i] = self._current
+            print(self)
+
+    def forward(self, num):
+        if self.i + abs(num) >= self._MAX:
+            print("Index requested out of bounds; please try again.")
+        else:
+            self.i += abs(num)
+            self.tab_data[:, self.i] = self._current
+            print(self)
+
+    def open(self, file):
+        '''
+        Open a pyTab file and determine if it should be appended or written
+        over.
+        '''
+        # This probably should just be implemented in the main function as a
+        # function itself. I should define a file convention.
+        pass
+
+    def save(self):
+        '''
+        Write the current tab data to file.
+        '''
+        pass
