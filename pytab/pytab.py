1417c2e772118018d207c144b7249299f350ef89
Matthew Bluteau <matthew.bluteau@gmail.com>
2019-05-13 21:33:49 -0300

    pyTab Project: pytab main program
    With the tab class in an acceptable state, the development of the main
    program for the CLI has been restarted. New arguments added for saving
    the tab file and quitting the program.

--- pytab/pytab.py
+++ pytab/pytab.py
@@ -37,32 +37,32 @@ def main():
 
     # TODO add an argument that can take in single letter chord names and
     # produce the correct tab representation
-    # TODO add argument to quit (asks to save or not)
-    # TODO add argument to save to file
-    parser.add_argument('-c', '--chord', nargs=6, choices=allowed, help='the'
-                        ' chord/fingering for the current count (if'
+    parser.add_argument('-c', '--chord', nargs=6, choices=allowed, help='The'
+                        ' chord/fingering for the current count. If'
                         ' present, there must be 6 positional arguments'
-                        '-one for each string). Input order runs from high '
-                        'e to low E')
-    parser.add_argument('-d', action='store_true', help='flag'
-                        ' to quit pyTab and save progress')
-    parser.add_argument('-p', action='store_true', help='flag'
-                        ' to print the entirety of the current tab')
-    parser.add_argument('-b', nargs='?', const=1, type=int, help='the'
+                        '--one for each string. Input order runs from high '
+                        'e to low E.')
+    parser.add_argument('-q', action='store_true', help='Flag'
+                        ' to quit pyTab and save progress.')
+    parser.add_argument('-p', action='store_true', help='Flag'
+                        ' to print the entirety of the current tab.')
+    parser.add_argument('-b', nargs='?', const=1, type=int, help='The'
                         ' number of counts to go back in the tab '
-                        '(the default is 1 if the flag is present)')
-    parser.add_argument('-f', nargs='?', const=1, type=int, help='the'
+                        '(the default is 1 if the flag is present).')
+    parser.add_argument('-f', nargs='?', const=1, type=int, help='The'
                         ' number of counts to go forward in the tab '
-                        '(the default is 1 if the flag is present)')
+                        '(the default is 1 if the flag is present).')
     parser.add_argument('-o', '--outfile', nargs=1, type=str,
-                        default=None, help='the file for the tab'
-                        ' to be written to')
+                        default=None, help='The file for the tab'
+                        ' to be written to.')
+    parser.add_argument('-s', '--save', nargs='?', type=str,
+                        const=True, help='Save the tab to file. Name of file'
+                        ' can be set as optional argument.')
 
     # Initialize the tab object and any other relevant variables (although
     # these should be soon implemented in the tab class itself) before entering
     # main program loop
     x = tab.Tab()
-    outfile = False
 
     while(True):
 
@@ -75,18 +75,20 @@ def main():
             continue
 
         if args.outfile is not None:
-            if os.path.isfile(args.outfile):
-                outfile = open(args.outfile, mode='a')
-            else:
-                outfile = open(args.outfile, mode='r')
-                outfile.write("Header for pyTab Tab\n\n")
+            x.set_info(filename=args.outfile)
 
-        if args.d:
+        # TODO test
+        if args.q:
             # write current tab buffer to file
-            if args.outfile is not None:
-                outfile.write('\n' + str(x))
-                outfile.close()
+            x.save_tab()
             break
+
+        # TODO test
+        if args.save:
+            if args.save is True:
+                x.save_tab()
+            else:
+                x.save_tab(filename=args.save)
 
         if args.p:
             pipe = os.popen('less', mode='w')
