4b3189814a909f0af9601de4dddfdc596f870cd1
Matthew Bluteau <matthew.bluteau@gmail.com>
2021-02-28 18:15:18 +0000

    Add test for valid Tab.backward() operation

--- guitab/tests/test_tab.py
+++ guitab/tests/test_tab.py
@@ -1,5 +1,6 @@
 from ..tab import Tab
 import pytest
+import datetime as dt
 
 
 @pytest.fixture
@@ -7,11 +8,32 @@ def blank_tab():
     return Tab()
 
 
-# TODO manually create a partly filled tab for testing (i.e. independent of
-# forward and backward methods)
 @pytest.fixture
-def filled_tab():
-    return Tab()
+def filled_tab_dict():
+    return {'_MAX': 78, 'clength': 6, '_blank':
+            ['-', '-', '-', '-', '-', '-'],
+            'tab_data':
+            [['-', '-', '-', '-', '-', '-'],
+             ['-', '-', '-', '-', '-', '-'],
+             ['-', '-', '-', '-', '-', '-'],
+             ['-', '-', '-', '-', '-', '-']],
+            '_leader': ('e|', 'B|', 'G|', 'D|', 'A|', 'E|'),
+            'allowed':
+            ['-', 'h', 'p', 'x', '0', '1', '2', '3', '4', '5', '6', '7',
+             '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
+             '18', '19', '20', '21', '22', '23', '24'],
+            'i': 3, 'imax': 3,
+            'info': {'filename': 'myTab.txt', 'title': 'My Tab',
+                     'author': 'Me', 'date': str(dt.date.today())}}
+
+
+@pytest.fixture
+def filled_tab(filled_tab_dict):
+    filled_tab = Tab()
+    filled_tab.tab_data = filled_tab_dict['tab_data']
+    filled_tab.i = filled_tab_dict['i']
+    filled_tab.imax = filled_tab_dict['imax']
+    return filled_tab
 
 
 def test_backward_out_of_bounds(blank_tab):
@@ -19,3 +41,10 @@ def test_backward_out_of_bounds(blank_tab):
     IndexError."""
     with pytest.raises(IndexError):
         blank_tab.backward()
+
+
+def test_backward_valid(filled_tab, filled_tab_dict):
+    """Check that valid `backward` operation yields expected tab state."""
+    filled_tab.backward()
+    filled_tab_dict['i'] = 2
+    assert filled_tab.__dict__ == filled_tab_dict
