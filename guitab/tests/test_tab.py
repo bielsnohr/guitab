f0cf45eb4a0fd21806e5799e9cb56f0a7c7d4ecd
Matthew Bluteau <matthew.bluteau@gmail.com>
2021-03-21 19:22:57 +0000

    Add a variety of tests for Tab class
    1. Finish off tests for `__str__()` method
    2. Add tests for `print()` method
    3. Add tests for missed case of negative input to `forward()` and `backward()`
    methods

--- guitab/tests/test_tab.py
+++ guitab/tests/test_tab.py
@@ -43,6 +43,13 @@ def test_backward_out_of_bounds(blank_tab):
         blank_tab.backward()
 
 
+def test_backward_negative_input(blank_tab):
+    """Check that passing negative number to `backward` operation yields
+    TypeError."""
+    with pytest.raises(TypeError):
+        blank_tab.backward(-1)
+
+
 def test_backward_valid(filled_tab, filled_tab_dict):
     """Check that valid `backward` operation yields expected tab state."""
     filled_tab.backward()
@@ -54,6 +61,13 @@ def test_forward_valid(blank_tab, filled_tab_dict):
     """Check that valid `forward` operation yields expected tab state."""
     blank_tab.forward(3)
     assert blank_tab.__dict__ == filled_tab_dict
+
+
+def test_forward_negative_input(blank_tab):
+    """Check that passing negative number to `backward` operation yields
+    TypeError."""
+    with pytest.raises(TypeError):
+        blank_tab.forward(-1)
 
 
 def test_write_current(filled_tab, filled_tab_dict):
@@ -88,22 +102,25 @@ def test_write_higher_index(filled_tab, filled_tab_dict):
     assert filled_tab.__dict__ == filled_tab_dict
 
 
-@pytest.mark.parametrize('invalid_tab', [
+@pytest.mark.parametrize('invalid_chord', [
     ['-', '-', '-', '-', '-', '-', '-'],
     ['r', 'z', '-', '-', '-', '-']
 ])
-def test_write_invalid_chord(invalid_tab, filled_tab):
+def test_write_invalid_chord(invalid_chord, filled_tab):
     """Check that writing an invalid chord raises a TypeError"""
     with pytest.raises(TypeError):
-        filled_tab.write(invalid_tab)
+        filled_tab.write(invalid_chord)
 
 
 def test_str_blank_tab(blank_tab):
+    """Confirm that the string representation of a blank tab is correct"""
     str_blank_tab = 'e|-\nB|-\nG|-\nD|-\nA|-\nE|-\n  *\n'
     assert str(blank_tab) == str_blank_tab
 
 
 def test_str_tab_1_row(blank_tab):
+    """Confirm that the string representation of a tab with 77 chords is still
+    in one row"""
     blank_tab.forward(77)
     str_tab_1_row = \
         'e|------------------------------------------------------------------------------\n'\
@@ -114,3 +131,112 @@ def test_str_tab_1_row(blank_tab):
         'E|------------------------------------------------------------------------------\n'\
         '                                                                               *\n'
     assert str(blank_tab) == str_tab_1_row
+
+
+def test_str_tab_2_rows(blank_tab):
+    """Confirm that the string representation of a tab with 78 chords goes into
+    2 rows"""
+    blank_tab.forward(78)
+    str_tab_2_rows = \
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '\n\n'\
+        'e|-\n'\
+        'B|-\n'\
+        'G|-\n'\
+        'D|-\n'\
+        'A|-\n'\
+        'E|-\n'\
+        '  *\n'
+    assert str(blank_tab) == str_tab_2_rows
+
+
+def test_str_tab_cursor_back_row_1(blank_tab):
+    """Confirm the string representation of a tab which has two rows but the
+    movement has brought the cursor back to the first row"""
+    blank_tab.forward(78)
+    blank_tab.backward(1)
+    str_tab_2_rows = \
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '                                                                               *\n\n'\
+        'e|-\n'\
+        'B|-\n'\
+        'G|-\n'\
+        'D|-\n'\
+        'A|-\n'\
+        'E|-\n'\
+        '\n'
+    assert str(blank_tab) == str_tab_2_rows
+
+
+def test_print_tab_blank(blank_tab, capfd):
+    """Confirm the internal print method for blank tab"""
+    str_blank_tab = 'e|-\nB|-\nG|-\nD|-\nA|-\nE|-\n  *\n\n'
+    blank_tab.print()
+    out, err = capfd.readouterr()
+    assert out == str_blank_tab
+
+
+def test_print_tab_2_rows(blank_tab, capfd):
+    """Confirm that the print method of a tab with 78 chords goes into 2
+    rows"""
+    str_tab_2_rows = \
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '\n\n'\
+        'e|-\n'\
+        'B|-\n'\
+        'G|-\n'\
+        'D|-\n'\
+        'A|-\n'\
+        'E|-\n'\
+        '  *\n\n'
+    blank_tab.forward(78)
+    blank_tab.print()
+    out, err = capfd.readouterr()
+    assert out == str_tab_2_rows
+
+
+def test_print_tab_3_total_rows(blank_tab, capfd):
+    """Confirm that the print method of a tab with more than 3 rows only prints
+    3 rows"""
+    str_tab_3_rows = \
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '\n\n'\
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '                                                                               *\n\n'\
+        'e|------------------------------------------------------------------------------\n'\
+        'B|------------------------------------------------------------------------------\n'\
+        'G|------------------------------------------------------------------------------\n'\
+        'D|------------------------------------------------------------------------------\n'\
+        'A|------------------------------------------------------------------------------\n'\
+        'E|------------------------------------------------------------------------------\n'\
+        '\n\n'
+    blank_tab.forward(389)  # create 5 rows
+    blank_tab.backward(78)  # go back 1 row
+    blank_tab.print()
+    out, err = capfd.readouterr()
+    assert out == str_tab_3_rows
