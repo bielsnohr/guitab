e388d92d8e15ebaf3138ce001785e696fc37ef6b
Matthew Bluteau <matthew.bluteau@gmail.com>
2021-04-04 12:09:44 +0100

    Add tests of guitab for moving forward into 2 rows, blank tab printing, and writing a C chord

--- guitab/tests/test_guitab.py
+++ guitab/tests/test_guitab.py
@@ -1,6 +1,7 @@
 from ..guitab import main
 import pytest
 import re
+from . import global_test_data
 
 
 welcome_message = "Welcome to guitab, an interactive command line program "\
@@ -26,11 +27,42 @@ def test_guitab_help_message(monkeypatch, capfd):
     """Confirm that the custom shell program displays a help message and
     then quits"""
 
-    # monkeypatch the "input" function, so that it returns predefined input.
-    # This simulates the user entering this input in the terminal
     user_input = iter(['-h', '-d'])
     monkeypatch.setattr('builtins.input', lambda _: next(user_input))
     main()
     out, err = capfd.readouterr()
     help_message_regex = re.compile(welcome_message + r"usage: guitab \[-h\]")
     assert help_message_regex.search(out)
+
+
+def test_guitab_print_tab_blank(monkeypatch, capfd):
+    """Confirm that the custom shell program displays a blank tab correctly and
+    then quits"""
+
+    user_input = iter(['-p', '-d'])
+    monkeypatch.setattr('builtins.input', lambda _: next(user_input))
+    main()
+    out, err = capfd.readouterr()
+    assert out == welcome_message + global_test_data.print_blank_tab
+
+
+def test_guitab_print_tab_2_rows(monkeypatch, capfd):
+    """Confirm that the custom shell program displays a 2 row tab correctly and
+    then quits"""
+
+    user_input = iter(['-f 78', '-d'])
+    monkeypatch.setattr('builtins.input', lambda _: next(user_input))
+    main()
+    out, err = capfd.readouterr()
+    assert out == welcome_message + global_test_data.print_tab_2_rows
+
+
+def test_guitab_write_chord(monkeypatch, capfd):
+    """Confirm that the custom shell program displays a blank tab correctly and
+    then quits"""
+
+    user_input = iter(['-c - 1 - 2 3 x', '-d'])
+    monkeypatch.setattr('builtins.input', lambda _: next(user_input))
+    main()
+    out, err = capfd.readouterr()
+    assert out == welcome_message + global_test_data.print_tab_c_chord
