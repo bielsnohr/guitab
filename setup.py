bbba47b95dee23014d38dc232aadbb6b5dbb62b5
Matthew Bluteau <matthew.bluteau@gmail.com>
2021-04-17 19:53:44 +0100

    Modify setup.py so that command-line script is packaged

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
         'console_scripts': [
-            # 'command = some.module:some_function',
+            'guitab = guitab.guitab:main',
         ],
     },
     include_package_data=True,
