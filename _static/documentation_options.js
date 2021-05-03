fa3bcce3fd137cccae3a6e8cbaa226dd50b762b1
bielsnohr <bielsnohr@users.noreply.github.com>
2021-05-03 16:46:08 +0000

    deploy: 5ef6c6ed18e0afae02cbe40a2d19efc28006eeeb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g7aca56a',
+    VERSION: '0.post1+g5ef6c6e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
